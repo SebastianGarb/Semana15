d851e5d38519314f341cf90b7adbed7af742a223
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-11-21 11:41:46 -0300

    commit

---
+++ Semana15/Program.cs
@@ -0,0 +1,27 @@
+﻿int fil = 5;
+int col = 3;
+int[][] mat = new int[fil][];
+
+for (int f = 0; f < mat.Length; f++)
+{
+    mat[f] = new int[col];
+}
+    
+for (int f = 0; f < mat.Length; f++)
+{
+    for (int c = 0; c < mat[0].Length; c++)
+    {
+        Console.WriteLine($"En fila {f}, ingrese numero {c}");
+        mat[f][c] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+for (int f = 0; f < mat.Length; f++)
+{
+    for (int c = 0; c < mat[0].Length; c++)
+    {
+        Console.Write($"{mat[f][c]}, ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
---
+++ matrizbidimencional/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+Random r = new Random();   
+int fil = 5, respuesta1;
+int col = 3, respuesta2;
+int[,] mat2 = new int[fil,col];
+
+Console.Write("Minimo: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Maximo: ");
+int max = int.Parse(Console.ReadLine());
+
+
+for (int f = 0; f < mat2.GetLength(0); f++)
+{
+    for (int c = 0; c < mat2.GetLength(1); c++)
+    {
+        
+        mat2[f, c] = r.Next(min,max+1);
+    }
+}
+
+for (int f = 0; f < mat2.GetLength(0); f++)
+{
+    for (int c = 0; c < mat2.GetLength(1); c++)
+    {
+        Console.Write($"{mat2[f,c]}, ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int f = 0; f < mat2.GetLength(0); f++)
+{
+    int sum = 0;
+    for (int c = 0; c < mat2.GetLength(1); c++)
+    {
+        sum += mat2[f, c];
+    }
+    Console.WriteLine($"La suma de fila {f} es: {sum}");
+}
+Console.WriteLine();
+
+for (int c = 0; c < mat2.GetLength(1); c++)
+{
+    int sum = 0;
+    for (int f = 0; f < mat2.GetLength(0); f++)
+    {
+        sum += mat2[f, c];
+    }
+    Console.WriteLine($"La suma de fila {c} es: {sum}");
+}
+Console.WriteLine();
+
+
+
+
+
+
